44447cd8d85a6c58e29147d70c6bf47fd5026a81
AlexRuEkb <a@pavlyuk.net>
2024-03-29 00:50:00 +0500

    5

--- Тема 3. Настройка потока данных/7. Подключение к Postres и изучение данных/Задание 1/realization.py
+++ Тема 3. Настройка потока данных/7. Подключение к Postres и изучение данных/Задание 1/realization.py
@@ -1,10 +1,21 @@
 from pyspark.sql import SparkSession
 
 spark = (
-        #
+        SparkSession.builder.appName("Read PostgreSQL Data")
+        .config("spark.jars.packages", "org.postgresql:postgresql:42.4.0")
+        .getOrCreate()
     )
 
-df = (spark.read
-        #
-        .load())
+jdbc_url = "jdbc:postgresql://rc1a-fswjkpli01zafgjm.mdb.yandexcloud.net:6432/de"
+connection_properties = {
+    "user": "student",
+    "password": "de-student",
+    "driver": "org.postgresql.Driver"
+}
+
+
+df = spark.read \
+        .jdbc(url=jdbc_url,
+              table="public.marketing_companies",
+              properties=connection_properties)
 df.count()
